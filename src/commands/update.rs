9d7bda46f256777fdf5358f76f38889ddc2f5dc9
Seth Samuel <sethfsamuel@gmail.com>
2024-05-06 21:58:30 -0400

    Update fix

--- src/commands/update.rs
+++ src/commands/update.rs
@@ -1,11 +1,13 @@
 use serenity::all::GetMessages;
 
+use crate::poll::consts::NUMBERS;
 use crate::poll::Poll;
 use crate::types::{Context, Error};
 
 #[poise::command(prefix_command)]
 pub async fn update(ctx: Context<'_>) -> Result<(), Error> {
     // println!(ctx);
+    let bot_id = ctx.http().get_current_user().await.unwrap().id;
 
     let thread = ctx
         .guild_channel()
@@ -14,11 +16,20 @@ pub async fn update(ctx: Context<'_>) -> Result<(), Error> {
         .messages(ctx.http(), GetMessages::new())
         .await
         .unwrap();
-    let thread_message = thread.last().unwrap().clone();
-    let poll = Poll::try_from(thread_message.content.clone()).unwrap();
+    let message = thread.last().unwrap().clone();
+    let mut poll = Poll::try_from(message.content.clone()).unwrap();
     println!("{:?}", poll);
+
     let _ = poll
-        .update_message(ctx.http(), ctx.channel_id(), thread_message.id)
+        .update_days(ctx.http(), bot_id, message.channel_id, message.id)
+        .await;
+    if poll.eliminated_days.len() == NUMBERS.len() {
+        println!("All days eliminated!");
+        poll.next_dates(ctx.http(), &message).await;
+    }
+
+    let _ = poll
+        .update_message(ctx.http(), ctx.channel_id(), message.id)
         .await;
 
     let _ = ctx.reply("Updated!").await;
