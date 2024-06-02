8d52d5fb6ed6abb8e77a8a74b1980b6b1ccf64dd
Seth Samuel <sethfsamuel@gmail.com>
2024-06-02 16:05:07 -0400

    WIP

--- src/nagger/mod.rs
+++ src/nagger/mod.rs
@@ -4,8 +4,8 @@ use std::{
     sync::Arc,
 };
 
-use chrono::{DateTime, Local};
-use serenity::all::{ChannelId, Http, MessageId};
+use chrono::{DateTime, Local, Utc};
+use serenity::all::{ChannelId, CreateMessage, Http, Message, MessageId};
 
 use crate::{discord::thread, telephone::Telephone, types::DiscordMessage};
 
@@ -61,25 +61,84 @@ impl Nagger {
 }
 
 impl Nagger {
-    pub async fn update(&mut self, from_message: DiscordMessage) {
-        if self.http.is_none() {
-            return;
-        }
+    pub async fn update(&mut self, from_message: DiscordMessage) -> Option<Message> {
+        self.http.as_ref()?;
         let http = self.http.as_ref().unwrap();
         if let Ok(message) = http
             .get_message(from_message.channel_id, from_message.message_id)
             .await
         {
             if thread::is_locked(http, message.channel_id).await {
-                return;
+                return None;
             }
             if let Some(telephone) = thread::try_from::<Telephone>(&message) {
                 self.messages.insert(from_message);
                 self.nagged_at.insert(from_message, telephone.nagged_at);
             }
+            return Some(message);
+        }
+        None
+    }
+
+    pub async fn execute(&mut self) {
+        for discord_message in self.messages.clone().iter() {
+            if Utc::now().timestamp_millis()
+                - self
+                    .nagged_at
+                    .get(discord_message)
+                    .unwrap_or(&chrono::offset::Local::now())
+                    .timestamp_millis()
+                > 1000 * 60 * 60 * 7
+            {
+                continue;
+            }
+            self.check_message(*discord_message).await;
         }
     }
-    pub async fn execute(&mut self) {
-        // self.http.get
+
+    pub async fn check_message(&mut self, discord_message: DiscordMessage) {
+        if let Some(message) = self.update(discord_message).await {
+            if let Some(telephone) = thread::try_from::<Telephone>(&message) {
+                if Utc::now().timestamp_millis() - telephone.nagged_at.timestamp_millis()
+                    > 1000 * 60 * 60 * 7
+                {
+                    self.nag(message, telephone).await;
+                }
+            }
+        }
+    }
+}
+
+impl Nagger {
+    pub async fn nag(&mut self, message: Message, mut telephone: Telephone) {
+        let user_id = telephone
+            .players
+            .iter()
+            .find(|id| !telephone.finished_players.contains(id));
+        if user_id.is_none() {
+            return;
+        }
+        _ = self
+            .http
+            .clone()
+            .unwrap()
+            .send_message(
+                message.channel_id,
+                vec![],
+                &CreateMessage::new().content(format!(
+                "Psst, <@{}> {}",
+                user_id.unwrap(),
+                "https://giphy.com/gifs/siliconvalleyhbo-watching-goodbye-window-26BRuo6sLetdllPAQ"
+            )),
+            )
+            .await;
+        telephone.nagged_at = Utc::now().into();
+        _ = thread::update(
+            &self.http.clone().unwrap(),
+            message.channel_id,
+            message.id,
+            telephone,
+        )
+        .await;
     }
 }
