6504b6c184727579f270eedd6b1b1e3499b304d6
Seth Samuel <sethfsamuel@gmail.com>
2025-10-03 11:41:42 -0400

    Message updates

--- src/commands/update.rs
+++ src/commands/update.rs
@@ -1,8 +1,9 @@
+use poise::CreateReply;
 use serenity::all::GetMessages;
 
 use crate::discord::thread;
 use crate::poll::consts::NUMBERS;
-use crate::poll::Poll;
+use crate::poll::{Poll, ReplyContext};
 use crate::telephone::Telephone;
 use crate::types::{Context, Error, OrpheusStatus};
 
@@ -13,9 +14,19 @@ pub async fn update(ctx: Context<'_>) -> Result<(), Error> {
     *status = OrpheusStatus::Processing;
     let bot_id = ctx.http().get_current_user().await.unwrap().id;
 
+    let reply = ctx.reply("Updating...").await.unwrap();
+
     let channel = ctx.guild_channel().await.unwrap();
 
     if channel.thread_metadata.unwrap().locked {
+        *status = OrpheusStatus::Waiting;
+        let _ = reply
+            .edit(
+                ctx,
+                CreateReply::default()
+                    .content("Thread currently being updated, please try again in a minute"),
+            )
+            .await;
         return Ok(());
     }
 
@@ -26,13 +37,25 @@ pub async fn update(ctx: Context<'_>) -> Result<(), Error> {
     let message = thread.last().unwrap().clone();
     if let Ok(mut poll) = Poll::try_from(message.content.clone()) {
         _ = poll
-            .update_days(ctx.http(), bot_id, message.channel_id, message.id, None)
+            .update_days(
+                ctx.http(),
+                bot_id,
+                message.channel_id,
+                message.id,
+                Some(ReplyContext {
+                    handle: &reply,
+                    ctx: &ctx,
+                }),
+            )
             .await;
         if poll.eliminated_days.len() == NUMBERS.len() {
             println!("All days eliminated!");
             poll.next_dates(ctx.http(), &message).await;
         }
 
+        let _ = reply
+            .edit(ctx, CreateReply::default().content("Updating thread..."))
+            .await;
         _ = poll
             .update_message(ctx.http(), ctx.channel_id(), message.id)
             .await;
@@ -42,7 +65,9 @@ pub async fn update(ctx: Context<'_>) -> Result<(), Error> {
             .await;
         _ = thread::update(ctx.http(), ctx.channel_id(), message.id, telephone).await;
     }
-    _ = ctx.reply("Updated!").await;
+    let _ = reply
+        .edit(ctx, CreateReply::default().content("Updated"))
+        .await;
 
     Ok(())
 }
